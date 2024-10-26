3167712c0e76b532824abd764bf67f6b7bdaf8df
pammikoundi <kpammi3@gatech.edu>
2024-10-26 13:23:21 -0400

    Add one more endpoint

--- back-end/ai/geminiConnection.py
+++ back-end/ai/geminiConnection.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 
 # Function calls the ai model and returns what is seen in the image
-def image_analysis_call(image, content_type):
+def image_analysis_call_alc(image):
     print("Analysis running")
     genai.configure(api_key="AIzaSyC7gKrKaJbpv9Tjn8d-4bQMUKCt9eOpin8")
     #print(f"{myfile=}")
@@ -16,5 +16,23 @@ def image_analysis_call(image, content_type):
     #print(f"{response.text=}")
 
     response = model.generate_content(["Return a list of all the alcohol bottles in the image. Return in format of bottle,type of alc,empty/not empty in json format","\n\n", image])
+    
+    return response
+
+# Function calls the ai model and returns what is seen in the image
+def image_analysis_call_nonalc(image):
+    print("Analysis running")
+    genai.configure(api_key="AIzaSyC7gKrKaJbpv9Tjn8d-4bQMUKCt9eOpin8")
+    #print(f"{myfile=}")
+
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    #response = model.generate_content("The opposite of hot is")
+    #print(response.text)
+
+    #response = model.generate_content(
+    #[myfile, "\n\n", "Can you tell me what mocktails I can make using the bottles in the image?"])
+    #print(f"{response.text=}")
+
+    response = model.generate_content(["Return a list of all the drink bottles in the image. Return in format of bottle,type of alc,empty/not empty in json format","\n\n", image])
     
     return response
--- back-end/routers/barPic.py
+++ back-end/routers/barPic.py
@@ -8,7 +8,7 @@ import json
 router = APIRouter()
 
 #Upload image and get gemimi response return.
-@router.post("/upload/", status_code=status.HTTP_201_CREATED)
+@router.post("/upload/alc", status_code=status.HTTP_201_CREATED)
 async def upload_file(source_file: UploadFile = File()):
 
     # Check file type and reject non image files   
@@ -18,7 +18,7 @@ async def upload_file(source_file: UploadFile = File()):
     image = Image.open(source_file.file)
     try:
         #Get ai response and parse for just the message
-        ai_response = geminiConnection.image_analysis_call(image, source_file.content_type).text
+        ai_response = geminiConnection.image_analysis_call_alc(image).text
 
     except:
         raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY, detail="AI response was unable to be retrieved and processed.")
@@ -28,3 +28,23 @@ async def upload_file(source_file: UploadFile = File()):
 
     return {"Response":ai_json}
 
+#Upload image and get gemimi response return.
+@router.post("/upload/nonalc", status_code=status.HTTP_201_CREATED)
+async def upload_file(source_file: UploadFile = File()):
+
+    # Check file type and reject non image files   
+    if(source_file.content_type.find("image/") == -1):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File type cannot be accepted")
+    # Try to read file if possible. If any errors return exception.  
+    image = Image.open(source_file.file)
+    try:
+        #Get ai response and parse for just the message
+        ai_response = geminiConnection.image_analysis_call_nonalc(image).text
+
+    except:
+        raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY, detail="AI response was unable to be retrieved and processed.")
+    ai_response = ai_response.replace("`","").replace("json","")
+    ai_json = json.loads(ai_response)
+    print(ai_response)
+
+    return {"Response":ai_json}
